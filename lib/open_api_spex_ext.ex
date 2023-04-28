92d8205b578e05cf4f8c13780109557127e24d58
Yurii Cherniavskyi <ych@webtrit.com>
2023-04-28 18:42:56 +0300

    feat: auto-generate titles for responses with model composition in operation
    Links:
    - https://swagger.io/docs/specification/data-models/inheritance-and-polymorphism/

--- lib/open_api_spex_ext.ex
+++ lib/open_api_spex_ext.ex
@@ -1,14 +1,8 @@
 defmodule OpenApiSpexExt do
   defmacro schema(body, opts \\ []) do
     quote do
-      module_name_parts = Module.split(__MODULE__)
-
-      # +2 to skip "Api" itself and "V*" part
-      drop_parts_amount = Enum.find_index(module_name_parts, &(&1 == "Api")) + 2
-
       title =
-        module_name_parts
-        |> Enum.drop(drop_parts_amount)
+        OpenApiSpexExt.api_module_name_parts(__MODULE__)
         |> Enum.map(&String.replace_suffix(&1, "Schema", ""))
         |> Enum.join("")
 
@@ -21,15 +15,68 @@ defmodule OpenApiSpexExt do
   @spec operation(action :: atom, spec :: map | keyword) :: any
   defmacro operation(action, spec) do
     quote do
-      suffix = __MODULE__ |> Module.split() |> List.last() |> String.replace_suffix("Controller", "")
+      spec = Map.new(unquote(spec))
 
-      prefix = Utils.String.camelize(unquote(action), :lower)
+      spec =
+        spec
+        |> OpenApiSpexExt.put_new_operation_id_in_operation(unquote(action), __MODULE__)
+        |> OpenApiSpexExt.put_new_title_to_error_responses_in_operation(unquote(action), __MODULE__)
 
-      operation_id = "#{prefix}#{suffix}"
-
-      spec_ext = Keyword.put(unquote(spec), :operation_id, operation_id)
-
-      OpenApiSpex.ControllerSpecs.operation(unquote(action), spec_ext)
+      OpenApiSpex.ControllerSpecs.operation(unquote(action), spec)
     end
+  end
+
+  def api_module_name_parts(module) do
+    module_name_parts = Module.split(module)
+
+    # +2 to skip "Api" itself and "V*" part
+    drop_parts_amount = Enum.find_index(module_name_parts, &(&1 == "Api")) + 2
+
+    Enum.drop(module_name_parts, drop_parts_amount)
+  end
+
+  def put_new_operation_id_in_operation(spec, action, module) do
+    action_parts = String.split(to_string(action), "_")
+    {prefix, action_parts} = List.pop_at(action_parts, -1)
+
+    {prefix, quantity} =
+      case prefix do
+        "index" -> {"get", "List"}
+        "show" -> {"get", ""}
+        prefix -> {prefix, ""}
+      end
+
+    suffix =
+      api_module_name_parts(module)
+      |> Enum.map(&String.replace_suffix(&1, "Controller", ""))
+      |> Enum.join("")
+
+    post_suffix = action_parts |> Enum.map(&String.capitalize/1) |> Enum.join()
+
+    operation_id = "#{prefix}#{suffix}#{post_suffix}#{quantity}"
+
+    Map.put_new(spec, :operation_id, operation_id)
+  end
+
+  def put_new_title_to_error_responses_in_operation(
+        %{operation_id: operation_id, responses: responses} = spec,
+        _action,
+        _module
+      )
+      when is_list(responses) or is_map(responses) do
+    responses =
+      Map.new(responses, fn
+        {status, {description, mime, %OpenApiSpex.Schema{title: nil, allOf: [common | _]} = schema}}
+        when is_atom(common) ->
+          title =
+            "#{Utils.String.camelize(operation_id)}#{Utils.String.camelize(status)}#{List.last(Module.split(common))}"
+
+          {status, {description, mime, %{schema | title: title}}}
+
+        element ->
+          element
+      end)
+
+    %{spec | responses: responses}
   end
 end
