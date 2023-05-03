3452e25ff2c374624377c40ae3339cc65cda039f
Yurii Cherniavskyi <ych@webtrit.com>
2023-05-03 20:19:31 +0300

    chore: import open_api_spex dependencу formatter configuration

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 [
-  import_deps: [:ecto, :ecto_sql, :phoenix],
+  import_deps: [:ecto, :ecto_sql, :phoenix, :open_api_spex],
   subdirectories: ["priv/*/migrations"],
   inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}", "priv/*/seeds.exs"],
   line_length: 120
