6a754ba871942832b3b7ced4d3ae9b7ebb4d09ae
Yurii Cherniavskyi <ych@webtrit.com>
2023-07-13 17:31:08 +0300

    feat(api): propagate 'x-request-id' to requests of adapter client

--- lib/webtrit_adapter_client.ex
+++ lib/webtrit_adapter_client.ex
@@ -18,6 +18,7 @@ defmodule WebtritAdapterClient do
         m -> [{Tesla.Middleware.BearerAuth, token: access_token} | m]
       end)
       |> then(&[{Tesla.Middleware.BaseUrl, base_url} | &1])
+      |> then(&[WebtritAdapterClient.Tesla.Middleware.RequestId | &1])
 
     Tesla.client(middleware)
   end
@@ -156,5 +157,36 @@ defmodule WebtritAdapterClient do
       {:error, reason} ->
         {:error, reason}
     end
+  end
+end
+
+defmodule WebtritAdapterClient.Tesla.Middleware.RequestId do
+  @behaviour Tesla.Middleware
+
+  require Logger
+
+  @impl true
+  def call(env, next, opts) do
+    IO.inspect(opts, label: "opts")
+    request_id_http_header = Keyword.get(opts, :http_header, "x-request-id")
+
+    request_id = Logger.metadata()[:request_id] || generate_request_id(opts)
+
+    env
+    |> Tesla.put_header(request_id_http_header, request_id)
+    |> Tesla.run(next)
+  end
+
+  defp generate_request_id(opts) do
+    request_id_prefix = Keyword.get(opts, :prefix, "WAC/")
+
+    # Inspired by Plug.RequestId
+    binary = <<
+      System.system_time(:nanosecond)::64,
+      :erlang.phash2({node(), self()}, 16_777_216)::24,
+      :erlang.unique_integer()::32
+    >>
+
+    request_id_prefix <> Base.url_encode64(binary)
   end
 end
