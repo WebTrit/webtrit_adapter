5cb40972ed0d94fef79ded13f8447632a7ce096e
Yurii Cherniavskyi <ych@webtrit.com>
2023-11-17 16:25:42 +0200

    chore: bump project patch version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WebtritAdapter.MixProject do
   def project do
     [
       app: :webtrit_adapter,
-      version: "0.5.3",
+      version: "0.5.4",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
