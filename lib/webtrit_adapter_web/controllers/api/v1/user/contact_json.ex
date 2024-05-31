3a9f0399a353631131a5ee62185c7b3c17384cef
Yurii Cherniavskyi <ych@webtrit.com>
2024-05-31 19:36:52 +0300

    refactor: improve assigns variable naming for clarity

--- lib/webtrit_adapter_web/controllers/api/v1/user/contact_json.ex
+++ lib/webtrit_adapter_web/controllers/api/v1/user/contact_json.ex
@@ -1,13 +1,13 @@
 defmodule WebtritAdapterWeb.Api.V1.User.ContactJSON do
   alias WebtritAdapterWeb.Api.V1.User.JSONMapping
 
-  def index(%{account_list: account_list, current_user: i_account}) do
+  def index(%{account_list: account_list, current_user_i_account: current_user_i_account}) do
     %{
-      items: for(account <- account_list, do: data(account, i_account))
+      items: for(account <- account_list, do: data(account, current_user_i_account))
     }
   end
 
-  defp data(account, current_user) do
+  defp data(account, current_user_i_account) do
     %{
       sip_status: JSONMapping.sip_status(account),
       numbers: %{
@@ -20,7 +20,7 @@ defmodule WebtritAdapterWeb.Api.V1.User.ContactJSON do
       alias_name: account["extension_name"],
       email: account["email"],
       company_name: account["companyname"],
-      is_current_user: account["i_account"] == current_user
+      is_current_user: account["i_account"] == current_user_i_account
     }
     |> Utils.Map.deep_filter_blank_values()
   end
