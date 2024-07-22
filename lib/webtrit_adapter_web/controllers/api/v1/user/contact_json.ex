1708a023bcd91756b7bd89315f88d97a713e0df9
Roman Yakimenko <r.yakimenko@webtrit.com>
2024-07-22 12:25:07 +0300

    feat: enhance contact list API response by adding user id property (#19)

--- lib/webtrit_adapter_web/controllers/api/v1/user/contact_json.ex
+++ lib/webtrit_adapter_web/controllers/api/v1/user/contact_json.ex
@@ -1,4 +1,5 @@
 defmodule WebtritAdapterWeb.Api.V1.User.ContactJSON do
+  alias WebtritAdapter.Mapper
   alias WebtritAdapterWeb.Api.V1.User.JSONMapping
 
   def index(%{account_list: account_list, current_user_i_account: current_user_i_account}) do
@@ -9,6 +10,8 @@ defmodule WebtritAdapterWeb.Api.V1.User.ContactJSON do
 
   defp data(account, current_user_i_account) do
     %{
+      user_id: Mapper.i_account_to_user_id(account["i_account"]),
+      is_current_user: account["i_account"] == current_user_i_account,
       sip_status: JSONMapping.sip_status(account),
       numbers: %{
         main: account["id"],
@@ -19,8 +22,7 @@ defmodule WebtritAdapterWeb.Api.V1.User.ContactJSON do
       last_name: account["lastname"],
       alias_name: account["extension_name"],
       email: account["email"],
-      company_name: account["companyname"],
-      is_current_user: account["i_account"] == current_user_i_account
+      company_name: account["companyname"]
     }
     |> Utils.Map.deep_filter_blank_values()
   end
