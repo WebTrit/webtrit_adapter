3be4b596c0ab12bc9e9660bff4db8aa4a0df3739
Yurii Cherniavskyi <ych@webtrit.com>
2024-10-01 12:50:51 +0300

    chore: bump project minor version and add alpha pre-release identifier

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WebtritAdapter.MixProject do
   def project do
     [
       app: :webtrit_adapter,
-      version: "0.9.0",
+      version: "0.10.0-alpha.1",
       elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
