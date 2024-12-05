6680a02add56d6375a57c916a7b0031bf79fc2e4
Yurii Cherniavskyi <ych@webtrit.com>
2024-12-05 11:51:37 +0200

    feat: clarify naming and description of added functionalities

--- lib/webtrit_adapter_web/controllers/api/v1/supported_functionality.ex
+++ lib/webtrit_adapter_web/controllers/api/v1/supported_functionality.ex
@@ -10,7 +10,7 @@ defmodule WebtritAdapterWeb.Api.V1.SupportedFunctionality do
       :recordings,
       :callHistory,
       :extensions,
-      :instantMessaging,
+      :internalMessaging,
       :smsMessaging
     ]
   end
@@ -22,8 +22,8 @@ defmodule WebtritAdapterWeb.Api.V1.SupportedFunctionality do
   def value_description(:recordings), do: "provides access to call recordings"
   def value_description(:callHistory), do: "provides access to call history"
   def value_description(:extensions), do: "retrieves the list of other users (contacts)"
-  def value_description(:instantMessaging), do: "provides access to sending and receiving messages"
-  def value_description(:smsMessaging), do: "provides access to sending and receiving text messages via SMS"
+  def value_description(:internalMessaging), do: "provides the ability to send and receive instant messages within the system (among system users)"
+  def value_description(:smsMessaging), do: "provides the ability to send and receive text messages using the Short Message Service (SMS)"
 
   def parse("signup"), do: :signup
   def parse("otpSignin"), do: :otpSignin
@@ -32,7 +32,7 @@ defmodule WebtritAdapterWeb.Api.V1.SupportedFunctionality do
   def parse("recordings"), do: :recordings
   def parse("callHistory"), do: :callHistory
   def parse("extensions"), do: :extensions
-  def parse("instantMessaging"), do: :instantMessaging
+  def parse("internalMessaging"), do: :internalMessaging
   def parse("smsMessaging"), do: :smsMessaging
   def parse(_), do: nil
 end
